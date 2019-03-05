75f28094d5438c53ab33cbbd5bfb65626ca5657e
anujp41 <anuj.pant41@gmail.com>
2019-03-05 16:42:56 -0500

    mvp of mvp of mvp doe

--- assets/js/index.js
+++ assets/js/index.js
@@ -10,35 +10,75 @@
     //if true means user has highlighted some text
     if (startOffset !== endOffset) {
       const modalText = modal.querySelector('.modal-text');
+      const modalDocSection = modal.querySelector('.icon-list');
       highlightedText = window.getSelection().toString();
       modalText.textContent = highlightedText;
+      const randNum = Math.ceil(Math.random() * 10);
+      console.log('randNum: ', randNum);
+      for (let i = 1; i <= randNum; i++) {
+        const googleDiv = document.createElement('div');
+        googleDiv.className = 'google-doc';
+        const googleImg = document.createElement('img');
+        const googleDocName = document.createElement('figcaption');
+        googleImg.src = '/assets/icons/doc.png';
+        googleDocName.textContent = `Google Document ${i}`;
+        googleDiv.appendChild(googleImg);
+        googleDiv.appendChild(googleDocName);
+        modalDocSection.appendChild(googleDiv);
+      }
       modalContainer.classList.add('show');
     }
+  }
+
+  //function to check if google doc was clicked
+  //check depends on fact that "google-doc" class has two children
+  function checkIfGoogleDoc(event) {
+    let result = false;
+    const { target } = event;
+    if (
+      target.className === 'google-doc' ||
+      target.parentNode.className === 'google-doc'
+    )
+      result = true;
+    return result;
+  }
+
+  //function to get name of clicked google doc
+  function getDocName(event) {
+    const { target } = event;
+    const docName =
+      target.textContent.trim() || target.parentNode.textContent.trim();
+    return docName;
   }
 
   //function to handle save to google doc
   function saveToGoogle(event) {
     //gets list of children of clicked item as the children have
     //to contain img and figcaption
-    const {
-      target: { children }
-    } = event;
-    console.log('children ', children);
+    const check = checkIfGoogleDoc(event);
     //checks if img exists
     //if true, means a google doc was clicked
     //if false, means was clicked inside modal but not on a google doc
-    if (children[0].localName === 'img') {
-      const googleDocName = children[1].textContent;
-      console.log(`
-      Text to Save: ${highlightedText};
-      Google Document Name: ${googleDocName};
-      URL: ${window.location.href}
+    if (check) {
+      const googleDocName = getDocName(event);
+      console.log(`Text to Save: ${highlightedText};\nGoogle Document Name: ${googleDocName};\nURL: ${
+        window.location.href
+      }
       `);
+      highlightedText = '';
+      modal.querySelector('.icon-list').innerHTML = '';
+      modalContainer.classList.remove('show');
     }
   }
 
   //event listener called when mouseup event fires
-  document.addEventListener('mouseup', getSelectedText);
+  document.addEventListener('mouseup', function(event) {
+    const { path: eventPath } = event;
+    const checkIfInModal = eventPath
+      .slice(0, -2)
+      .some(child => child.classList.contains('modal'));
+    if (!checkIfInModal) getSelectedText();
+  });
 
   //event listener to remove modal when user click outside
   document.addEventListener('mousedown', event => {
@@ -47,6 +87,8 @@
       .slice(0, -2)
       .some(child => child.classList.contains('modal'));
     if (!checkIfInModal) {
+      highlightedText = '';
+      modal.querySelector('.icon-list').innerHTML = '';
       modalContainer.classList.remove('show');
     }
   });
