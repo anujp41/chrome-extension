6b42bb23931f72261a8bb9aa9be7f98ef98767d2
anujp41 <anuj.pant41@gmail.com>
2019-03-05 14:12:04 -0500

    updated modal transitions

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,10 +1,39 @@
 (function() {
-  document.addEventListener('mouseup', getSelectedText);
+  // variable to reference modal
+  const modalContainer = document.querySelector('.modal-container');
+  const modal = document.querySelector('.modal');
+
+  //event listener called when mouseup event fires
+  document.addEventListener('mouseup', function() {
+    if (!modalContainer.classList.contains('show')) {
+      getSelectedText();
+    }
+  });
 
   function getSelectedText() {
     const { startOffset, endOffset } = window.getSelection().getRangeAt(0);
-    //if block true means user has highlighted some text
-    if (startOffset === endOffset) console.log('nothing highlighted');
-    else console.log('selected text: ', window.getSelection().toString());
+    //if true means user has highlighted some text
+    if (startOffset === endOffset) {
+      console.log('nothing highlighted');
+    } else {
+      modal.textContent = window.getSelection().toString();
+      modalContainer.classList.add('show');
+    }
   }
+
+  document.addEventListener('mousedown', event => {
+    if (modalContainer.classList.contains('show')) {
+      const {
+        srcElement: { classList }
+      } = event;
+      if (classList.contains('modal')) {
+        console.log('in modal');
+      } else {
+        console.log('outside modal');
+        modalContainer.classList.remove('show');
+      }
+    } else {
+      console.log('no modal on page');
+    }
+  });
 })();
